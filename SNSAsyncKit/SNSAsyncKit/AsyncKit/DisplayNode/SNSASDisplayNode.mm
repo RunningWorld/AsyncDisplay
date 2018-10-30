b175920523c1a85e201a3483effefd0ddc86addc
Lee <lidong.cld@alibaba-inc.com>
2018-10-30 15:24:36 +0800

    D:【修改编译问题】

--- SNSAsyncKit/SNSAsyncKit/AsyncKit/DisplayNode/SNSASDisplayNode.mm
+++ SNSAsyncKit/SNSAsyncKit/AsyncKit/DisplayNode/SNSASDisplayNode.mm
@@ -9,6 +9,7 @@
 #import "SNSASDisplayNode.h"
 #import "SNSASDisplayNodeInternal.h"
 #import "SNSASDisplayLayer.h"
+#import "SNSASDisplayNode+FrameworkPrivate.h"
 
 @implementation SNSASDisplayNode
 
@@ -47,6 +48,57 @@
 //        ASDisplayNodeAssert(ASObjectIsEqual(_cachedSubnodes, _subnodes), @"Expected _subnodes and _cachedSubnodes to have the same contents.");
     }
     return _cachedSubnodes ?: @[];
+}
+
+- (ASHierarchyState)hierarchyState
+{
+    ASDN::MutexLocker l(__instanceLock__);
+    return _hierarchyState;
+}
+
+- (void)setHierarchyState:(ASHierarchyState)newState
+{
+    ASHierarchyState oldState = ASHierarchyStateNormal;
+    {
+        ASDN::MutexLocker l(__instanceLock__);
+        if (_hierarchyState == newState) {
+            return;
+        }
+        oldState = _hierarchyState;
+        _hierarchyState = newState;
+    }
+    
+    // Entered rasterization state.
+//    if (newState & ASHierarchyStateRasterized) {
+//        ASDisplayNodeAssert(checkFlag(Synchronous) == NO, @"Node created using -initWithViewBlock:/-initWithLayerBlock: cannot be added to subtree of node with subtree rasterization enabled. Node: %@", self);
+//    }
+//
+//    // Entered or exited range managed state.
+//    if ((newState & ASHierarchyStateRangeManaged) != (oldState & ASHierarchyStateRangeManaged)) {
+//        if (newState & ASHierarchyStateRangeManaged) {
+//            [self enterInterfaceState:self.supernode.pendingInterfaceState];
+//        } else {
+//            // The case of exiting a range-managed state should be fairly rare.  Adding or removing the node
+//            // to a view hierarchy will cause its interfaceState to be either fully set or unset (all fields),
+//            // but because we might be about to be added to a view hierarchy, exiting the interface state now
+//            // would cause inefficient churn.  The tradeoff is that we may not clear contents / fetched data
+//            // for nodes that are removed from a managed state and then retained but not used (bad idea anyway!)
+//        }
+//    }
+//
+//    if ((newState & ASHierarchyStateLayoutPending) != (oldState & ASHierarchyStateLayoutPending)) {
+//        if (newState & ASHierarchyStateLayoutPending) {
+//            // Entering layout pending state
+//        } else {
+//            // Leaving layout pending state, reset related properties
+//            ASDN::MutexLocker l(__instanceLock__);
+//            _pendingTransitionID = ASLayoutElementContextInvalidTransitionID;
+//            _pendingLayoutTransition = nil;
+//        }
+//    }
+//
+//    ASDisplayNodeLogEvent(self, @"setHierarchyState: %@", NSStringFromASHierarchyStateChange(oldState, newState));
+//    as_log_verbose(ASNodeLog(), "%s%@ %@", sel_getName(_cmd), NSStringFromASHierarchyStateChange(oldState, newState), self);
 }
 
 @end
